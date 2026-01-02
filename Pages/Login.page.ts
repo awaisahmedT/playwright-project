b8c0b4cfe2327fee844123b955f1b2999e5c7273
awaisahmedT <62584732+awaisahmedT@users.noreply.github.com>
2026-01-02 07:17:32 +0500

    Update Login.page.ts

--- Pages/Login.page.ts
+++ Pages/Login.page.ts
@@ -1,5 +1,5 @@
 import { Locator, Page } from '@playwright/test';
-import { BasePage } from './base.page';
+import { BasePage } from './base.page.ts';
 
 // Handles all login page interactions
 export class LoginPage extends BasePage {
